3137ef5b71c340bd9513ac0c28003bfe203b4910
Jakub Kopański <jkopanski@protonmail.com>
2019-01-13 17:50:01 +0100

    enable basic flycheck

---
+++ modules/mad-flycheck.el
@@ -0,0 +1,17 @@
+;;; modules/mad-flycheck.el --- Flycheck -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Install basic Flycheck.
+
+;;; Code:
+
+(require 'mad-package)
+
+(use-package "flycheck"
+  :commands (flycheck-mode flycheck-list-errors flycheck-buffer)
+  :config
+  (setq flycheck-check-syntax-automatically '(save mode-enabled)))
+
+(provide 'mad-flycheck)
+;;; mad-flycheck.el ends here
