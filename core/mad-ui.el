6afceeb7023e54c745dea8311436458a0d64f29d
Jakub Kopański <jkopansk@gmail.com>
2019-02-19 11:19:54 +0100

    hightlight line mode

--- core/mad-ui.el
+++ core/mad-ui.el
@@ -12,6 +12,13 @@
 (when (fboundp 'tool-bar-mode)   (tool-bar-mode   -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
+(add-hook 'prog-mode-hook 'hl-line-mode)
+(add-hook 'text-mode-hook 'hl-line-mode)
+(add-hook 'conf-mode-hook 'hl-line-mode)
+
+(setq hl-line-sticky-flag nil
+      global-hl-line-sticky-flag nil)
+
 (provide 'mad-ui)
 
 ;;; mad-ui.el ends here
