381c214977fd6b76a47b65d499547e9921de42ba
Jakub Kopański <jkopanski@protonmail.com>
2019-03-16 21:34:19 +0100

    Delete not mine comments

--- modules/mad-git.el
+++ modules/mad-git.el
@@ -7,8 +7,6 @@
 
 (require 'mad-core)
 
-;; Invoke Magit by typing C-x g, and you can thank me later.
-;; See http://magit.github.io/ for instructions.
 (use-package "magit"
   :commands magit-status
   :bind ("C-x g" . magit-status)
@@ -17,11 +15,8 @@
 
 (use-package "forge")
 
-;; Use M-x gist-buffer or M-x gist-region to create a gist
-;; directly from the current buffer or selection.
 (use-package "gist")
 
-;; Mark uncommitted changes in the fringe.
 (use-package "git-gutter-fringe"
   :delight
   :config
