b32eb0a27cb92694e1102cf97e193f5e42f827dc
Jakub Kopański <jkopanski@protonmail.com>
2019-08-22 10:15:58 +0200

    RTL mode using my own lsp

--- modules/mad-rtl.el
+++ modules/mad-rtl.el
@@ -6,10 +6,20 @@
 ;;; Code:
 
 (require 'mad-core)
+(require 'mad-lsp)
 
 (use-package "firrtl-mode"
   :mode "\\.fir$"
   :mode "\\.firrtl$")
+
+(use-package "verilog-mode"
+  :mode "\\.[ds]?vh?$"
+  :hook (verilog-mode . mad-lsp))
+
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-stdio-connection "vlsp")
+                  :major-modes '(verilog-mode)
+                  :server-id 'vlsp))
 
 (provide 'mad-rtl)
 ;;; mad-rtl.el ends here
