e20839c992ba12af4f02650bba39b8283984f7b9
Jakub Kopański <jakub@famisoft.pl>
2020-08-11 09:43:22 +0200

    Enable org-roam

--- modules/mad-org.el
+++ modules/mad-org.el
@@ -53,5 +53,18 @@
         org-hide-leading-stars nil
         org-superstar-leading-bullet ?\s))
 
+(use-package org-roam
+  :ensure t
+  :hook (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory "~/roam")
+  :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n g" . org-roam-graph-show))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))
+              (("C-c n I" . org-roam-insert-immediate))))
+
 (provide 'mad-org)
 ;;; mad-org.el ends here
